5f86f68826a86ebe7fb8c55a9a4e3d995879cbc3
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-21 13:10:05 +0530

    profile

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1="Harsha Vardhan"
+name2="Siva Kumar"
+
+echo "$name1: Hello how are you?"
+echo "$name2: I am great, what are you upto?"
+echo "$name1: I got a DevOps Job recently"
+echo "$name2: Great to hear that, congratulations $name1"
