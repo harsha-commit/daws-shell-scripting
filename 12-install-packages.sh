d51837707f83236b825a142201e2b9cc3d8f5652
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-21 18:03:56 +0530

    profile

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -20,7 +20,7 @@ do
     echo "Package to install: $i"
     dnf list installed $i &>> $LOGFILE
 
-    if [ $? -ne 0 ]
+    if [ $? -eq 0 ]
     then
         echo -e "$i already installed...$Y SKIPPED $W"
     else
