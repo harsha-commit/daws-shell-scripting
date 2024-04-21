0c484705549b18f5942bbd2a51eadc2029a3a0fb
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-21 13:13:31 +0530

    profile

---
+++ 03-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=$1
+name2=$2
+
+echo "$name1: Hello how are you?"
+echo "$name2: I am great, what are you upto?"
+echo "$name1: I got a DevOps Job recently"
+echo "$name2: Great to hear that, congratulations $name1"
