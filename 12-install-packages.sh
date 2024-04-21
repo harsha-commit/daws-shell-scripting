5730c11fb6cc21f822a1458e75e1098668c48140
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-21 17:58:22 +0530

    profile

---
+++ 12-install-packages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d '.' -f 1)
+LOGFILE="/tmp/$SCRIPTNAME-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as super user"
+    exit 1
+fi
+
+for i in $@
+do
+    echo "Package to install: $i"
+    dnf installed list $i &>> $LOGFILE
+
+    if [ $? -ne 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPED $W"
+    else
+        echo "$i installation started..."
+    fi
+
+done
