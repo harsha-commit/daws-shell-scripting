b9cee88de0ae957dd271a0acb1a30bfe7f6533fd
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-21 17:22:00 +0530

    profile

--- 10-functions.sh
+++ 10-functions.sh
@@ -12,10 +12,10 @@ W="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2...$R FAILED"
+        echo -e "$2...$R FAILED $W"
         exit 1
     else
-        echo -e "$2...$G SUCCESS"
+        echo -e "$2...$G SUCCESS $W"
     fi
 }
 
